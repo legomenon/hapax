35ced91f246db07589c35bd9dba30fa9ada447ca
legomenon <solomovsky.roman@gmail.com>
2023-02-19 11:43:55 +0200

    add pretty printing

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,17 @@ use std::path::Path;
 
 #[derive(Debug)]
 struct Stats {
-    stats: HashMap<String, usize>,
+    term_frequency: HashMap<String, usize>,
     file: String,
+    length: usize,
 }
 
 fn main() {
-    let file = "data";
+    let file = "ga.srt";
     let words = find_words(file);
     let st = get_stats(&words, file);
     write_stats(&st);
-    dbg!(st);
+    // dbg!(st);
 }
 
 fn read_lines<P: AsRef<Path>>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> {
@@ -23,7 +24,7 @@ fn read_lines<P: AsRef<Path>>(filename: P) -> io::Result<io::Lines<io::BufReader
 }
 
 fn find_words(file: &str) -> Vec<String> {
-    let re = regex::Regex::new(r#"\b[\p{L}’'-]{2,}\b"#).unwrap();
+    let re = regex::Regex::new(r#"\b[\p{L}’'-]+\b"#).unwrap();
     let lines = read_lines(file).unwrap();
 
     lines
@@ -33,7 +34,7 @@ fn find_words(file: &str) -> Vec<String> {
         })
         .map(|l| {
             re.find_iter(&l)
-                .map(|w| w.as_str().to_owned())
+                .map(|w| w.as_str().to_owned().to_lowercase())
                 .collect::<Vec<String>>()
         })
         .collect::<Vec<Vec<String>>>()
@@ -44,6 +45,7 @@ fn find_words(file: &str) -> Vec<String> {
 
 fn get_stats(words: &Vec<String>, file: &str) -> Stats {
     let mut stats: HashMap<String, usize> = HashMap::new();
+    let len = words.len();
     words.into_iter().for_each(|w| {
         stats
             .entry(w.clone())
@@ -52,22 +54,34 @@ fn get_stats(words: &Vec<String>, file: &str) -> Stats {
     });
 
     Stats {
-        stats,
+        term_frequency: stats,
         file: file.to_owned(),
+        length: len,
     }
 }
 
 fn write_stats(s: &Stats) {
     let file_name = s.file.to_owned() + ".stats";
     let file = File::create(file_name).unwrap();
-    let mut vec: Vec<(&String, &usize)> = s.stats.iter().collect();
+    let mut vec: Vec<(&String, &usize)> = s.term_frequency.iter().collect();
     vec.sort_by(|a, b| b.1.cmp(&a.1));
 
     let mut file = BufWriter::new(file);
+    let head = format!(
+        "{:<22} {:<22}{}\n{}\n",
+        "WORD:",
+        "FREQUENCY:",
+        "PERCENT:",
+        "-".repeat(53)
+    );
+    file.write(head.as_bytes()).unwrap();
 
     let s = vec
         .iter()
-        .map(|(key, value)| format!("{:<20} {}\n", key, *value))
+        .map(|(key, value)| {
+            let per = **value as f64 * 100.0 / s.length as f64;
+            format!("{:<22} {:<22} {:.2}%\n", key, *value, per)
+        })
         .collect::<String>();
     file.write_all(s.as_bytes()).unwrap();
 }
