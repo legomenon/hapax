f59a179e584519815f6f9c3be62f7b30ec70c1b1
legomenon <solomovsky.roman@gmail.com>
2023-02-24 16:06:42 +0200

    cli refactoring

--- src/cli.rs
+++ src/cli.rs
@@ -3,11 +3,11 @@ use hapax::{find_files_in_dir, find_words_in_file, lemmanization, Stats};
 use std::io;
 use std::path::PathBuf;
 use std::str::FromStr;
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 
 use rayon::prelude::*;
 
-#[derive(Parser)]
+#[derive(Parser, Clone)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
     /// type of the output file: json/csv/text
@@ -18,10 +18,10 @@ struct Cli {
     #[clap(default_value = "./")]
     #[arg(short, long)]
     path: String,
-    /// skip removing junk words
+    /// exclude junk words [default: false]
     #[arg(short, long)]
     junk: bool,
-    /// skip words lemmanization
+    /// words lemmanization [default: false]
     #[arg(short, long)]
     lemma: bool,
 
@@ -29,13 +29,13 @@ struct Cli {
     command: Commands,
 }
 
-#[derive(Subcommand)]
+#[derive(Subcommand, Clone)]
 enum Commands {
     /// provides term frequency TF
     Tf {
-        /// file for parsing
-        #[arg(short, long)]
-        file: Option<String>,
+        /// files for parsing
+        #[arg(short, long,value_parser, num_args = 1.., value_delimiter = ' ')]
+        file: Option<Vec<String>>,
         /// dir for parsing
         #[arg(short, long, conflicts_with("file"))]
         dir: Option<String>,
@@ -55,142 +55,115 @@ enum Output {
 }
 
 fn main() -> io::Result<()> {
-    let cli = Cli::parse();
+    let cli = Arc::new(Cli::parse());
 
-    match cli.command {
+    match &cli.command {
         Commands::Tf { dir, file } => match (dir, file) {
             (_, Some(f)) => {
-                let mut words = find_words_in_file(&f).unwrap_or(Vec::new());
-                if !cli.lemma {
-                    words = lemmanization(words)?;
-                }
-
-                let f = PathBuf::from(f);
-                if words.is_empty() {
-                    println!(
-                        "{:<16}{} is empty | can not be read",
-                        "WARNING",
-                        f.file_name()
-                            .expect("file name is invalid")
-                            .to_string_lossy()
-                    );
-                    std::process::exit(1);
-                }
-                println!(
-                    "{:<14} {}",
-                    "PARSING",
-                    f.file_name()
-                        .expect("file name is invalid")
-                        .to_string_lossy()
-                );
-                let mut st = Stats::new(&words, &f);
-
-                let o = cli
-                    .output
-                    .parse::<Output>()
-                    .expect("can not parse cli command");
-                if !cli.junk {
-                    st.exclude_junk("./junk_words.txt");
-                }
-
-                match st.write(&format!("{o:?}"), &cli.path) {
-                    Ok(_) => println!("{:<15}{}", "OK", st.file_name),
-                    Err(e) => println!("{:<15}{}:{}", "ERROR", st.file_name, e),
-                }
+                let files: Vec<PathBuf> = f.iter().map(PathBuf::from).collect();
+                process_files(&files, cli)
             }
             (Some(d), _) => {
                 let files = find_files_in_dir(d)?;
-                let o = cli
-                    .output
-                    .parse::<Output>()
-                    .expect("can not parse cli command");
-
-                println!("Parsing {} files:\n\n", files.len());
-
-                files.par_iter().for_each(|f| {
-                    let mut words =
-                        find_words_in_file(&f.display().to_string()).unwrap_or(Vec::new());
-
-                    if !cli.lemma {
-                        words = lemmanization(words).unwrap_or(Vec::new());
-                    }
-                    if words.is_empty() {
-                        println!(
-                            "{:<16}{} is empty | can not be read",
-                            "WARNING",
-                            f.file_name()
-                                .expect("file name is invalid")
-                                .to_string_lossy()
-                        );
-                        return;
-                    }
-
-                    println!(
-                        "{:<14} {}",
-                        "PARSING",
-                        f.file_name()
-                            .expect("file name is invalid")
-                            .to_string_lossy()
-                    );
-                    let mut st = Stats::new(&words, f);
-                    if !cli.junk {
-                        st.exclude_junk("./junk_words.txt");
-                    }
-
-                    match st.write(&format!("{o:?}"), &cli.path) {
-                        Ok(_) => println!("{:<15}{}", "OK", st.file_name),
-                        Err(e) => println!("{:<15}{}:{}", "ERROR", st.file_name, e),
-                    }
-                });
+                process_files(&files, cli)
             }
             (None, None) => eprintln!("Provide file path or directory"),
         },
         Commands::Tft { dir: tdir } => {
             let files = find_files_in_dir(tdir)?;
-            let o = cli
-                .output
-                .parse::<Output>()
-                .expect("can not parse cli command");
-
-            println!("PARSING {} FILES:\n", files.len());
-            let st = Mutex::new(Stats::new_total());
-            files.par_iter().for_each(|f| {
-                let mut words = find_words_in_file(&f.display().to_string()).unwrap_or(Vec::new());
-
-                if !cli.lemma {
-                    words = lemmanization(words).unwrap_or(Vec::new());
-                }
-
-                if words.is_empty() {
-                    println!(
-                        "{:<16}{} is empty | can not be read",
-                        "WARNING",
-                        f.file_name()
-                            .expect("file name is invalid")
-                            .to_string_lossy()
-                    );
-                    return;
-                }
-                println!(
-                    "{:<15} {}",
-                    "PARSING",
-                    f.file_name()
-                        .expect("file name is invalid")
-                        .to_string_lossy()
-                );
-                st.lock().unwrap().extend(&words);
-            });
-            let mut st = st.lock().unwrap();
-            if !cli.junk {
-                st.exclude_junk("./junk_words.txt");
-            }
-            match st.write(&format!("{o:?}"), &cli.path) {
-                Ok(_) => println!("\n\n{:<15}{}", "OK", st.file_name),
-                Err(e) => println!("\n\n{:<15}{}:{}", "ERROR", st.file_name, e),
-            }
+            process_files_total(&files, cli);
         }
     }
     Ok(())
+}
+
+fn process_files(files: &Vec<PathBuf>, cli: Arc<Cli>) {
+    let cli = cli;
+    println!("PARSING {} FILES:\n", files.len());
+    files.par_iter().for_each(|f| {
+        let mut words = find_words_in_file(&f.display().to_string()).unwrap_or(Vec::new());
+
+        if !cli.lemma {
+            words = lemmanization(words).unwrap_or(Vec::new());
+        }
+        if words.is_empty() {
+            println!(
+                "{:<15}{} not exist | can not be read",
+                "WARNING",
+                f.file_name()
+                    .expect("file name is invalid")
+                    .to_string_lossy()
+            );
+            return;
+        }
+
+        println!(
+            "{:<14} {}",
+            "PARSING",
+            f.file_name()
+                .expect("file name is invalid")
+                .to_string_lossy()
+        );
+        let mut st = Stats::new(&words, f);
+        if !cli.junk {
+            st.exclude_junk("./junk_words.txt");
+        }
+        let o = cli
+            .output
+            .parse::<Output>()
+            .expect("can not parse cli command");
+
+        match st.write(&format!("{o:?}"), &cli.path) {
+            Ok(_) => println!("{:<15}{}", "OK", st.file_name),
+            Err(e) => println!("{:<15}{}:{}", "ERROR", st.file_name, e),
+        }
+    });
+}
+
+fn process_files_total(files: &Vec<PathBuf>, cli: Arc<Cli>) {
+    let cli = cli;
+
+    println!("PARSING {} FILES:\n", files.len());
+    let st = Mutex::new(Stats::new_total());
+    files.par_iter().for_each(|f| {
+        let mut words = find_words_in_file(&f.display().to_string()).unwrap_or(Vec::new());
+
+        if !cli.lemma {
+            words = lemmanization(words).unwrap_or(Vec::new());
+        }
+
+        if words.is_empty() {
+            println!(
+                "{:<16}{} is empty | can not be read",
+                "WARNING",
+                f.file_name()
+                    .expect("file name is invalid")
+                    .to_string_lossy()
+            );
+            return;
+        }
+        println!(
+            "{:<15} {}",
+            "PARSING",
+            f.file_name()
+                .expect("file name is invalid")
+                .to_string_lossy()
+        );
+        st.lock().unwrap().extend(&words);
+    });
+
+    let mut st = st.lock().unwrap();
+    if !cli.junk {
+        st.exclude_junk("./junk_words.txt");
+    }
+    let o = cli
+        .output
+        .parse::<Output>()
+        .expect("can not parse cli command");
+    match st.write(&format!("{o:?}"), &cli.path) {
+        Ok(_) => println!("\n\n{:<15}{}", "OK", st.file_name),
+        Err(e) => println!("\n\n{:<15}{}:{}", "ERROR", st.file_name, e),
+    }
 }
 
 impl FromStr for Output {
